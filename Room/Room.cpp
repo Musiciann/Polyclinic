605a0e00146a8693302b18f492f65e59e42591b2
Musiciann <144940569+Musiciann@users.noreply.github.com>
2024-10-15 20:24:25 +0300

    Update Room.cpp

--- Room/Room.cpp
+++ Room/Room.cpp
@@ -16,7 +16,7 @@ Room::Room(){}
 
 Room::Room(unsigned short RoomNumber, float Length, float Width, float Height):RoomNumber(RoomNumber), Length(Length), Width(Width), Height(Height){}
 
-bool Room::AppropriateSize(){
+virtual bool Room::AppropriateSize(){
     if(FindSize() >= 12){
         return true;
     }
